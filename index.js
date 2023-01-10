c49fe93c9d7770f7548408efee05640708c39206
Ganesh <gsw7517@gmail.com>
2023-01-10 15:54:05 +0530

    The variables in javascript

---
+++ index.js
@@ -0,0 +1,33 @@
+// ! three types of varibles 
+
+var  a;
+// *  In this case multiple declaration is allowed
+// * it can be reassign
+// * var have the global scope 
+
+
+let ab ="new let variable";
+// * In this case multiple declaration is not allowed
+// * it can be resign 
+// * it is block scope variable so it don't have global variable
+
+const av=0;
+// * not changable;
+// * It can be reasign
+// * const also is block scope variable
+
+
+if(1==1){
+    var ax = "gsw" 
+}
+console.log(ax);
+
+
+for(let d=0;d<9;d++){
+    document.write(d);
+}
+document.write(d+11)
+// This above line will not works as let have block level scope
+
+
+// !---------- Variable section ends here 
